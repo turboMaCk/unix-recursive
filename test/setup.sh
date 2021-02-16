6dd3182a2d7c91f928c65c58f25541d6c15f699b
Marek Fajkus <marek.faj@gmail.com>
2021-02-16 22:00:12 +0100

    Pack workdir (#9)

--- test/setup.sh
+++ test/setup.sh
@@ -1,5 +1,7 @@
 #! /usr/bin/env bash
 
+tar xvf test/workdir.tar.gz
+
 sudo mkdir test/workdir/only-roots-dir1
 sudo touch test/workdir/only-roots-dir1/roots-file1
 sudo chmod 700 test/workdir/only-roots-dir1
